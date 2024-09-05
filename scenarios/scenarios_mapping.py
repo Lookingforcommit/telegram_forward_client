dff5e7225ab362479062c41c4fad6cc78f595841
synthimental <138771075+synthimental@users.noreply.github.com>
2024-09-05 19:17:50 +0300

    Update scenarios_mapping.py

--- scenarios/scenarios_mapping.py
+++ scenarios/scenarios_mapping.py
@@ -8,6 +8,7 @@ from scenarios.scenarios_classes.base import BaseScenario
 from scenarios.scenarios_classes.text_deletion import TextDeletionScenario
 from scenarios.scenarios_classes.text_insertion import TextInsertionScenario
 from scenarios.scenarios_classes.text_replacement import TextReplacementScenario
+from scenarios.scenarios_classes.regex_empty import RegexEmptyScenario
 
 OBJECTS_MAPPING: Dict[str, BaseScenario] = {
     "sample_scenario": SampleScenario(),
@@ -15,4 +16,5 @@ OBJECTS_MAPPING: Dict[str, BaseScenario] = {
     "text_deletion": TextDeletionScenario(),
     "text_insertion": TextInsertionScenario(),
     "links_deletion": TextLinksDeletionScenario(),
+    "regex_empty":  RegexEmptyScenario(),
 }
